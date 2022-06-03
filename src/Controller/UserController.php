d44412901d20884a3640a527010e9b29c1d7e748
Jeffrey GEROMEGNACE <jeffrey.geromegnace@gmail.com>
2022-06-03 15:45:18 +0200

    Ajout page streamer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,13 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/', name: 'app_user')]
     public function index(): Response
     {
         return $this->render('user/user.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
+
+    #[Route('/streamer', name: 'app_streamer')]
+    public function streamer(): Response
+    {
+        $streamer = 'streamer';
+        return $this->render('user/streamer.html.twig', [
+
+        ]);
+    }
+
 }
