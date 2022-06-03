a1995fb948224c31e8b74952424095a0aa7e32f3
Melvin Delorme <melvin.delorme12@gmail.com>
2022-06-03 11:54:48 +0200

    Header Complet sans liens de page

--- migrations/Version20220603083240.php
+++ migrations/Version20220603083240.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220602143355 extends AbstractMigration
+final class Version20220603083240 extends AbstractMigration
 {
     public function getDescription(): string
     {
