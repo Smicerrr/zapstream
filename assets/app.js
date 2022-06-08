50ce3d250c764037498963aaea54bc1c1e910777
Melvin Delorme <melvin.delorme12@gmail.com>
2022-06-08 09:08:30 +0200

    Page actu complete

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import './styles/formRegister.css';
+import './styles/actus.css';
 
 
 // start the Stimulus application
