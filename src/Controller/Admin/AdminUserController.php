3d9f258b996fc8a978c2887534a3cfa85301da9e
Jeffrey GEROMEGNACE <jeffrey.geromegnace@gmail.com>
2022-06-13 09:26:11 +0200

    ajout admin

--- src/Controller/Admin/AdminUserController.php
+++ src/Controller/Admin/AdminUserController.php
@@ -1,18 +1,26 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin/user')]
 class AdminUserController extends AbstractController
 {
-    #[Route('/admin/user', name: 'app_admin_user')]
-    public function index(): Response
+    #[Route('/', name: 'app_admin_user_index')]
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'AdminUserController',
+        $currentUser = $this->getUser();
+
+        $users = $userRepository->findAll();
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $users,
+            'currentUser' => $currentUser,
         ]);
     }
 }
